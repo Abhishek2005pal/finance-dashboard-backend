4cb60bf540ce2c499ce62a6df6f0129db4afc4ba
Abhishek Pal <a9315296730@gmail.com>
2026-04-02 17:24:37 +0530

    Modfied in package.js

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
+import path from 'path';
 import { z } from 'zod';
 
-dotenv.config();
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+} else {
+  dotenv.config();
+}
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
